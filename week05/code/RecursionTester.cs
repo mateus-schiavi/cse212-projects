fec3f8cabea39ac0cf6b2b0b7fff9b714f64aa98
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2024-05-23 15:54:55 -0300

    fixed RecursionTester

--- week05/code/RecursionTester.cs
+++ week05/code/RecursionTester.cs
@@ -147,7 +147,12 @@ public static class RecursionTester {
     /// </summary>
     public static int SumSquaresRecursive(int n) {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+        {
+            return 0;
+        }
+            return n*n + SumSquaresRecursive(n - 1);
+
     }
 
     /// <summary>
